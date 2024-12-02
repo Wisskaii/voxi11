b2943e9b7f074e0e7c2b4a1ff84cb5f2a10a016b
Wisskaii <144198486+Wisskaii@users.noreply.github.com>
2024-12-02 11:50:08 +0100

    Add files via upload

--- voxi11/src/fr/voxi/eval/AudioBook.java
+++ voxi11/src/fr/voxi/eval/AudioBook.java
@@ -1,5 +1,7 @@
 package fr.voxi.eval;
 
+import java.util.*;
+
 public class AudioBook {
 	
 	private int numero ;
@@ -17,6 +19,45 @@ public class AudioBook {
 	public String toString() {
 		return "AudioBook [numero=" + numero + ", titre=" + titre + ", gratuit=" + gratuit + "]";
 	}
+
+	private Map<Auditeur, Evaluation> evaluations = new HashMap<Auditeur, Evaluation>() ;
+	
+	public boolean evaluer(Auditeur auditeur, Evaluation evaluation) {
+		if(evaluations.containsKey(auditeur) == false) {
+			evaluations.put(auditeur, evaluation) ;
+			return true ;
+		}
+		else {
+			return false ;
+		}
+	}
+	
+	public void afficherEvaluations() {
+		for(Auditeur auditeur : evaluations.keySet()){
+			System.out.println(auditeur + "" + evaluations.get(auditeur));
+		}
+	}
+	
+	public void supprimerEvaluation(Auditeur auditeur){
+		if(evaluations.containsKey(auditeur)){
+			System.out.println(evaluations.get(auditeur)) ;
+			evaluations.remove(auditeur) ;
+		}
+		else{
+			System.out.println("ERREUR : L'auditeur n'a pas évaluer l'AudioBook !");
+		}
+	}
+	
+	public boolean aEvalue(Auditeur auditeur){
+		if(evaluations.containsKey(auditeur) == true){
+			return true ;
+		}
+		else {
+			return false ;
+		}
+	}
+	
+	// Faire exo 5
 	
 	/**
 	 * @return the numero
